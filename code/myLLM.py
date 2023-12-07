192db854d888e9d5f4141cac56a8bc2258290d46
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-12-07 14:55:51 +0530

    add embeddings.

--- code/myLLM.py
+++ code/myLLM.py
@@ -4,23 +4,34 @@ from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.embeddings import HuggingFaceInstructEmbeddings # Embeddings
 from langchain.vectorstores import FAISS # for VectorDB
 from logging_and_exception import logger , CustomException
+import os
 
-chat = GooglePalm(google_api_key=apiKey,temperature=0.8,verbose=True)
+# chat = GooglePalm(google_api_key=apiKey,temperature=0.8,verbose=True)
 
-response = chat("""Act as a PPT creator for fees structure for IITM Bs Degree students.
-                create a professional powerpoint presentation please show the complete fees in different slides
-                for all categories because the main aim is to show the fees of IITM""")
+# response = chat("""Act as a PPT creator for fees structure for IITM Bs Degree students.
+#                 create a professional powerpoint presentation please show the complete fees in different slides
+#                 for all categories because the main aim is to show the fees of IITM""")
 
-# load data
-loader = CSVLoader(file_path=r'DB/data.csv',source_column='prompt')
-data = loader.load()
-# print(data)
-# print(response)
+# # load data
+# loader = CSVLoader(file_path=r'DB/data.csv',source_column='prompt')
+# data = loader.load()
+# # print(data)
+# # print(response)
 
 try:
-    embeddings = HuggingFaceInstructEmbeddings(
-        query_instruction="Represent the query for retrieval: "
+    os.makedirs(name='embeddingTransformers',exist_ok=True)
+    model_name = "hkunlp/instructor-large"
+    model_kwargs = {'device': 'cpu'}
+    encode_kwargs = {'normalize_embeddings': True}
+    hf = HuggingFaceInstructEmbeddings(
+        cache_folder = r'embeddingTransformers',
+        model_name=model_name,
+        model_kwargs=model_kwargs,
+        encode_kwargs=encode_kwargs
     )
+    
+    vector
+
 
 except Exception as e:
     raise CustomException(e)
