7ca2117b68fae78352a806a01ed39afb987ccf4a
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-12-07 13:42:21 +0530

    add all.

--- code/myLLM.py
+++ code/myLLM.py
@@ -1,3 +1,11 @@
 from secret.secret import apiKey
+from langchain.llms import GooglePalm
 
-print(apiKey)
+chat = GooglePalm(google_api_key=apiKey,temperature=0.8)
+
+response = chat("""Act as a PPT creator for fees structure for IITM Bs Degree students.
+                create a professional powerpoint presentation please show the complete fees in different slides
+                for all categories because the main aim is to show the fees of IITM""")
+
+
+print(response)
