94190f931b9d250eea36c1a2ef5156020d45157d
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-12-08 10:40:08 +0530

    add

--- code/myLLM.py
+++ code/myLLM.py
@@ -6,17 +6,17 @@ from langchain.vectorstores import FAISS # for VectorDB
 from logging_and_exception import logger , CustomException
 import os
 
-# chat = GooglePalm(google_api_key=apiKey,temperature=0.8,verbose=True)
+chat = GooglePalm(google_api_key=apiKey,temperature=0.8,verbose=True)
 
-# response = chat("""Act as a PPT creator for fees structure for IITM Bs Degree students.
-#                 create a professional powerpoint presentation please show the complete fees in different slides
-#                 for all categories because the main aim is to show the fees of IITM""")
+response = chat("""Act as a PPT creator for fees structure for IITM Bs Degree students.
+                create a professional powerpoint presentation please show the complete fees in different slides
+                for all categories because the main aim is to show the fees of IITM""")
 
-# # load data
-# loader = CSVLoader(file_path=r'DB/data.csv',source_column='prompt')
-# data = loader.load()
-# # print(data)
-# # print(response)
+# load data
+loader = CSVLoader(file_path=r'DB/data.csv',source_column='prompt')
+data = loader.load()
+# print(data)
+# print(response)
 
 try:
     os.makedirs(name='embeddingTransformers',exist_ok=True)
@@ -30,7 +30,8 @@ try:
         encode_kwargs=encode_kwargs
     )
     
-    vector
+    vectorDB = FAISS.from_documents(documents=data,embedding=hf)
+    vectorDB.save_local(r'DB')
 
 
 except Exception as e:
